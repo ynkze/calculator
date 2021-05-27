2bef90ac3851d7c3bd0f8f99a07e245dfbaa3f8a
ynkze <teyyiheng@gmail.com>
2021-05-27 08:43:34 -0700

    First commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function operate(a, operand, b){
+    switch(operand){
+        case "+":
+            add(a,b);
+            break;
+        case "-":
+            subtract(a,b);
+            break;
+        case "*":
+            multiply(a,b);
+            break;
+        case "/":
+            divide(a,b);
+            break;
+    }
+}
